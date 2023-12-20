35fd0f540bec7aefd2dd1d3934a1117dda5b5cb3
“xubiying” <2094197975@qq.com>
2023-12-20 13:36:53 +0800

    feat: add put and delete fetcher, adjust post fetcher

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -2,7 +2,6 @@ import type { Response } from "./vite-env";
 
 const prefix = "http://127.0.0.1:8080/api";
 export async function getFetcher(key: string) {
-
   const resp = (await fetch(prefix + key, { mode: "cors" }).then((res) =>
     res.json()
   )) as Response<any>;
@@ -13,6 +12,7 @@ export async function getFetcher(key: string) {
   console.log(resp);
   return resp.data;
 }
+
 export async function postFetcher(
   key: string,
   //注：Record类型用于创建一个具有指定属性类型的新对象类型
@@ -29,5 +29,37 @@ export async function postFetcher(
     throw new Error(resp.errorCode + ": " + resp.errorMessage);
   }
   console.log(resp);
-  return resp.data;
+  return resp;
+}
+
+export async function putFetcher(
+  key: string,
+  //注：Record类型用于创建一个具有指定属性类型的新对象类型
+  body: { arg: Record<string, unknown> | Array<unknown> }
+) {
+  const resp = (await fetch(prefix + key, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body.arg),
+    mode: "cors",
+  }).then((res) => res.json())) as Response<any>;
+
+  if (!resp.success) {
+    throw new Error(resp.errorCode + ": " + resp.errorMessage);
+  }
+  console.log(resp);
+  return resp;
+}
+//和GET相似，区别是返回是否成功的布尔值
+export async function deleteFetcher(key: string) {
+  const resp = (await fetch(prefix + key, {
+    method: "DELETE",
+    mode: "cors",
+  }).then((res) => res.json())) as Response<any>;
+
+  if (!resp.success) {
+    throw new Error(resp.errorCode + ": " + resp.errorMessage);
+  }
+  console.log(resp);
+  return resp.success;
 }
