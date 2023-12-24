d756228b12270586545554a7c9c86ced327325d2
parameterN <2055156343@qq.com>
2023-12-24 15:09:10 +0800

    1. 修改AI总结接口返回信息 2. 添加词云图停用词

--- backend/src/main/java/team/semg04/themirroroflaw/search/LawFrequencyAnalyzer.java
+++ backend/src/main/java/team/semg04/themirroroflaw/search/LawFrequencyAnalyzer.java
@@ -3,7 +3,11 @@ package team.semg04.themirroroflaw.search;
 import com.kennycason.kumo.WordFrequency;
 import com.kennycason.kumo.nlp.FrequencyAnalyzer;
 import com.kennycason.kumo.nlp.tokenizers.ChineseWordTokenizer;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +18,7 @@ public class LawFrequencyAnalyzer {
         final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
         frequencyAnalyzer.setWordFrequenciesToReturn(100);
         frequencyAnalyzer.setMinWordLength(2);
+        frequencyAnalyzer.setStopWords(getStopWordList());
         frequencyAnalyzer.setWordTokenizer(new ChineseWordTokenizer());
         List<WordFrequency> wordFrequencies = frequencyAnalyzer.load(List.of(text));
         List<Map<String, Object>> rst = new ArrayList<>();
@@ -21,5 +26,19 @@ public class LawFrequencyAnalyzer {
             rst.add(Map.of("name", word.getWord(), "value", word.getFrequency()));
         }
         return rst;
+    }
+
+    public static List<String> getStopWordList() {
+        List<String> stopWords = new ArrayList<>();
+        ClassPathResource resource = new ClassPathResource("stopwords.txt");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                stopWords.add(line);
+            }
+        } catch (IOException e) {
+//            e.printStackTrace();
+        }
+        return stopWords;
     }
 }
